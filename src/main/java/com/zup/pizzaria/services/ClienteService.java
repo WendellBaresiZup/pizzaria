8f71e160d8b10e33938f931e87cab95e68e0d637
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 16:31:15 -0300

    Criado o metodo listarClientes no pcote service. Esse metodo vai listar os clientes que tem no banco de dados.

--- src/main/java/com/zup/pizzaria/services/ClienteService.java
+++ src/main/java/com/zup/pizzaria/services/ClienteService.java
@@ -5,6 +5,8 @@ import com.zup.pizzaria.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClienteService {
 
@@ -13,5 +15,10 @@ public class ClienteService {
 
     public Cliente cadastrarCliente(Cliente cliente){
         return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarClientes(){
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes;
     }
 }
