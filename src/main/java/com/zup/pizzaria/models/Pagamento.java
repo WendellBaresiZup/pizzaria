1a6a31cf07249a21899d999fd8f71524e7777c6c
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 15:41:53 -0300

    Injetando os beans dentro da classe Pagamento para identificiar que ela é uma entidade. Implementando no atributo ID que ele vai ser um tipo ID e que vai ser gerado automaticamente

--- src/main/java/com/zup/pizzaria/models/Pagamento.java
+++ src/main/java/com/zup/pizzaria/models/Pagamento.java
@@ -1,9 +1,18 @@
 package com.zup.pizzaria.models;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+
+import java.time.LocalDateTime;
+@Entity
 public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long pedidoId;
     private FormaPagamento formaPagamento;
     private double valorPago;
