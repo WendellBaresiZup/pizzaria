46d657caeece126d743cf4c49e3f272e027d8583
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 18:11:25 -0300

    Criado as validações na metodo criarPedido

--- src/main/java/com/zup/pizzaria/controllers/PedidoController.java
+++ src/main/java/com/zup/pizzaria/controllers/PedidoController.java
@@ -1,13 +1,12 @@
 package com.zup.pizzaria.controllers;
 
 import com.zup.pizzaria.dtos.PedidoDTO;
+import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.models.Pedido;
 import com.zup.pizzaria.services.PedidoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,11 +20,20 @@ public class PedidoController {
     }
 
     @PostMapping
-    public ResponseEntity<PedidoDTO> criarPedido(@RequestBody Pedido pedido) {
-        PedidoDTO pedidoDTO = pedidoService.criarPedido(pedido);
-        return ResponseEntity.ok(pedidoDTO);
+    public ResponseEntity<Object> criarPedido(@RequestBody Pedido pedido) {
+        if (pedido == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro: O pedido não pode ser nulo!!");
+        }
+        PedidoDTO pedidoCriado = pedidoService.criarPedido(pedido);
+        if (pedido != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("O Pedido foi criado com sucesso!!");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar pedido!!");
+        }
     }
 
+    @GetMapping
     public ResponseEntity<List<Pedido>> listarPedidos(){
         List<Pedido> pedidos = pedidoService.listarPedidos();
         return ResponseEntity.ok(pedidos);
