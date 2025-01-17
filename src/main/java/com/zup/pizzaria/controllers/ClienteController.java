d81e5d002814113f1a2fb22992975ef62fa76d68
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 15:34:31 -0300

    Injetando os beans dentro da classe ClienteController para designar o que essa classe vai ser um controlador

--- src/main/java/com/zup/pizzaria/controllers/ClienteController.java
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -2,11 +2,18 @@ package com.zup.pizzaria.controllers;
 
 import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ClienteController {
 
+    @Autowired
     private ClienteService clienteService;
 
+    @PostMapping("/clientes")
     public Cliente cadastrarCliente(Cliente cliente){
         return clienteService.cadastrarCliente(cliente);
     }
