5c9f81b055297fb068a0e60761c4999003d187dc
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 14:54:29 -0300

    Criação da Classe Pagamento no pacote models

---
+++ src/main/java/com/zup/pizzaria/models/Pagamento.java
@@ -0,0 +1,54 @@
+package com.zup.pizzaria.models;
+
+import java.time.LocalDateTime;
+
+public class Pagamento {
+    private Long id;
+    private Long pedidoId;
+    private String formaPagamento;
+    private double valorPago;
+    private LocalDateTime dataHoraPagamento;
+
+    public Pagamento() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public LocalDateTime getDataHoraPagamento() {
+        return dataHoraPagamento;
+    }
+
+    public void setDataHoraPagamento(LocalDateTime dataHoraPagamento) {
+        this.dataHoraPagamento = dataHoraPagamento;
+    }
+}
