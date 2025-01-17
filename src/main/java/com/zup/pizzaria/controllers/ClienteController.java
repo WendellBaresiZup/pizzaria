4ff6bfb03911380ca914e5efeffc478061d96215
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 16:40:49 -0300

    Criado o metodo listarClientes no pacote controller. Esse metodo vai gerar o controle e listar os clientes que tem no banco de dados.

--- src/main/java/com/zup/pizzaria/controllers/ClienteController.java
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -5,10 +5,9 @@ import com.zup.pizzaria.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping (value = ("/clientes"))
@@ -28,5 +27,11 @@ public class ClienteController {
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar cliente!!");
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarClientes(){
+        List<Cliente> clientes = clienteService.listarClientes();
+        return ResponseEntity.ok(clientes);
     }
 }
