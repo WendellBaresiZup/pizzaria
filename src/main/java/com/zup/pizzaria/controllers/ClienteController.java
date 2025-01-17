b0715e66902029d05a16a4602a52c95f755bb261
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 16:28:14 -0300

    Implementado as validações para a cadastrar o cliente dentro do banco de dados

--- src/main/java/com/zup/pizzaria/controllers/ClienteController.java
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -3,7 +3,10 @@ package com.zup.pizzaria.controllers;
 import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,15 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @PostMapping
-    public Cliente cadastrarCliente(Cliente cliente){
-        return clienteService.cadastrarCliente(cliente);
+    public ResponseEntity<Object> cadastrarCliente(@RequestBody Cliente cliente) {
+        if (cliente == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro: O nome do cliente não pode ser nulo!!");
+        }
+        Cliente clienteCadastrado = clienteService.cadastrarCliente(cliente);
+        if (cliente != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("O Cliente foi criado com sucesso!!");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar cliente!!");
+        }
     }
 }
