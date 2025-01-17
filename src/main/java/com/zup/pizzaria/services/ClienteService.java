a5ad1962a5e77c82dc837fe234d14bd4d7edc435
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-17 15:28:08 -0300

    Injetando os beans dentro da classe ClienteService para designar o que essa classe vai ser um serviço

--- src/main/java/com/zup/pizzaria/services/ClienteService.java
+++ src/main/java/com/zup/pizzaria/services/ClienteService.java
@@ -2,9 +2,13 @@ package com.zup.pizzaria.services;
 
 import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClienteService {
 
+    @Autowired
     private ClienteRepository clienteRepository;
 
     public Cliente cadastrarCliente(Cliente cliente){
