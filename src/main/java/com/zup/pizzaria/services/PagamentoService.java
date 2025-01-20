4e3445fc7526e75a2d39863709757ea90e39c501
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-20 10:20:31 -0300

    Instanciando as interfaces dessa da classe PegamentoService

--- src/main/java/com/zup/pizzaria/services/PagamentoService.java
+++ src/main/java/com/zup/pizzaria/services/PagamentoService.java
@@ -1,8 +1,15 @@
 package com.zup.pizzaria.services;
 
+import com.zup.pizzaria.models.Pagamento;
 import com.zup.pizzaria.repository.PagamentoRepository;
+import com.zup.pizzaria.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PagamentoService {
 
+    @Autowired
     private PagamentoRepository pagamentoRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
 }
