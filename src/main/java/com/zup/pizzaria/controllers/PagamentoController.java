f06768b7fefb23e7019be3fc24b19296729ff191
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-20 15:00:22 -0300

    Criado o metodo cadastrarPagamento no pacote controller

--- src/main/java/com/zup/pizzaria/controllers/PagamentoController.java
+++ src/main/java/com/zup/pizzaria/controllers/PagamentoController.java
@@ -1,5 +1,25 @@
 package com.zup.pizzaria.controllers;
 
+import com.zup.pizzaria.models.Pagamento;
+import com.zup.pizzaria.services.PagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/pagamentos")
 public class PagamentoController {
+
+    @Autowired
+    private PagamentoService pagamentoService;
+
+    @PostMapping
+    public ResponseEntity<Pagamento> cadastrarPagamento(@RequestBody Pagamento pagamento){
+        Pagamento pagamentoCadastrado = pagamentoService.cadastarPagamento(pagamento);
+        return ResponseEntity.ok(pagamentoCadastrado);
+    }
 
 }
