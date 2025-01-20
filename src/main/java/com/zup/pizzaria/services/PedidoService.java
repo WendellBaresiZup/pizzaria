8aabe4af757986200b3cfd97a9405e75ea019abd
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-20 08:56:49 -0300

    Validacoes implementadas na classe PedidoService dentro do pacote service

--- src/main/java/com/zup/pizzaria/services/PedidoService.java
+++ src/main/java/com/zup/pizzaria/services/PedidoService.java
@@ -5,6 +5,7 @@ import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.models.Pedido;
 import com.zup.pizzaria.repository.ClienteRepository;
 import com.zup.pizzaria.repository.PedidoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class PedidoService {
     }
 
     public PedidoDTO criarPedido(Pedido pedido) {
-        // Salva pedido
+        validacoesPedido(pedido);
         pedidoRepository.save(pedido);
 
         // Obtenho cliente
@@ -35,5 +36,17 @@ public class PedidoService {
     public List<Pedido> listarPedidos(){
         List<Pedido> pedidos = pedidoRepository.findAll();
         return pedidos;
+    }
+
+    public ResponseEntity<String> validacoesPedido(Pedido pedido){
+        if (pedido == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro: O pedido não pode ser nulo!!");
+        }
+        if (pedido != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("O Pedido foi criado com sucesso!!");
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao cadastrar pedido!!");
+        }
     }
 }
