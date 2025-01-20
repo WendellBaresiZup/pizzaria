be0a483a04f73bb89e9a04ddf58e3058bccc5cb3
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-20 10:15:48 -0300

    Criado a classe PagamentoService no pacote service

---
+++ src/main/java/com/zup/pizzaria/services/PagamentoService.java
@@ -0,0 +1,8 @@
+package com.zup.pizzaria.services;
+
+import com.zup.pizzaria.repository.PagamentoRepository;
+
+public class PagamentoService {
+
+    private PagamentoRepository pagamentoRepository;
+}
