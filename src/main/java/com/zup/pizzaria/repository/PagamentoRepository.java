bb72a30d8a4cdf7f700efcb51ceb7fd4646aeebe
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-01-22 14:25:00 -0300

    Criado o metodo deleteByPedidoId vazio dentro da interface Pagamento repository para não acontecer de delete em modelo cascata

--- src/main/java/com/zup/pizzaria/repository/PagamentoRepository.java
+++ src/main/java/com/zup/pizzaria/repository/PagamentoRepository.java
@@ -4,4 +4,5 @@ import com.zup.pizzaria.models.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+    void deleteByPedidoId(Long pedidoId);
 }
